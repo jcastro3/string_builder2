1853b0a2392e60764596d8026891e48a3a0025d6
Jesus Castro <jcastro@nearsoft.com>
2014-07-22 17:24:36 -0700

    added test for array and function as parameter all passing

--- src/sb_test.js
+++ src/sb_test.js
@@ -13,20 +13,67 @@ describe('___String Builder___', function () {
     });
 
     describe('Cat Method', function(){
-        it('should store one value in buffer', function(){
-            var storage = sb.cat('hello');
-            expect(storage.buffer).toMatch(['hello']);
-        });
-        it('should store multiple cat values in buffer', function() {
-            var storage = sb.cat('Javascript', 'is', 'sexy').cat('!');
-            expect(storage.buffer).toMatch(['Javascript','is','sexy','!']);
+
+        describe('string as parameter', function() {
+
+
+            it('should store one value in buffer', function(){
+                var storage = sb.cat('hello');
+                expect(storage.buffer).toMatch(['hello']);
+            });
+            it('should store multiple values in buffer', function() {
+                var storage = sb.cat('Javascript', 'is', 'sexy').cat('!');
+                expect(storage.buffer).toMatch(['Javascript','is','sexy','!']);
+            });
+
+            it('should be empty', function() {
+               var storage = sb.cat(),
+                   empty = [];
+                expect(storage.buffer).toMatch(empty);
+            });
+
         });
 
-        it('should be empty', function() {
-           var storage = sb.cat(),
-               empty = [];
-            expect(storage.buffer).toMatch(empty);
+        describe('function as parameter', function() {
+
+
+            it('should store one function value', function(){
+                var storage = sb.cat(function() { return 'hello';});
+                expect(storage.buffer).toMatch(['hello']);
+            });
+            it('should store multiple function values', function() {
+                var storage = sb.cat('Javascript', function(){ return 'is';}, 'sexy').cat(function(){return '!';});
+                expect(storage.buffer).toMatch(['Javascript','is','sexy','!']);
+            });
+
         });
+
+        describe('array as parameter', function() {
+
+
+            it('should store one array value in buffer', function(){
+                var storage = sb.cat(['hello']);
+                expect(storage.buffer).toMatch(['hello']);
+            });
+            it('should store multiple array values in buffer', function() {
+                var storage = sb.cat(['Javascript', 'is', 'sexy']).cat(['!']);
+                expect(storage.buffer).toMatch(['Javascript','is','sexy','!']);
+            });
+
+        });
+
+        describe('all combined', function() {
+
+
+            it('should store array, function values in buffer', function(){
+                var storage = sb.cat(['im an array'], 'im a string')
+                                .cat('im another concat string', function(){ return 'with a function'});
+                expect(storage.buffer).toMatch(['im an array','im a string','im another concat string','with a function']);
+            });
+
+
+        });
+
 
     });
 });
